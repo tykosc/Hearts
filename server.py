fb5ed3acb5927f1c9384ddb671e2b25ab47bc771
Rina London <rml2196@barnard.edu>
2022-05-02 13:42:21 -0400

    Add feedback in quiz results

--- server.py
+++ server.py
@@ -10,10 +10,18 @@ from util import *
 
 #int of correct anwers from user in quiz
 score = 0
+play_score = 0
+trick_score = 0
+mc_score = 0
+
 
 def reset_score():
    global score
    score = 0
+   global play_score, trick_score, mc_score
+   play_score = 0
+   trick_score = 0
+   mc_score = 0
 
 # stores the answer to an active question, if necessary
 answer = {}
@@ -58,7 +66,20 @@ def quiz_end(difficulty):
       num_questions = test_init_1["step_count"] 
    else: 
       num_questions = test_init_0["step_count"]
-   return render_template('quiz_end.html', quiz_score = quiz_score, num_questions= num_questions)
+
+   if quiz_score == num_questions:
+      grade = "You got an A+ ! You're ready to kill it in Hearts!"
+   elif (.9 * float(num_questions)) <= quiz_score < num_questions:
+      grade = "You got an A ! You're ready to go out and play!"
+   elif (.8 * float(num_questions))<= quiz_score <num_questions:
+      grade = "You got a B ! You're ready to jump in a game of Hearts, but you might want some seasoned players to help along the way!"
+   elif(.7 * float(num_questions))<= quiz_score <num_questions:
+      grade = "You got a C ! Not bad, but you might want to brush up on the rules."
+   elif(.6 * float(num_questions))<= quiz_score <num_questions:
+      grade = "You got a D ! Don't feel bad, try again after brushing up on the rules."
+   else:
+      grade = "You failed the quiz, have you visited the Learn page yet?"
+   return render_template('quiz_end.html', quiz_score = quiz_score, num_questions= num_questions, trick_score = trick_score, play_score = play_score, mc_score = mc_score, grade = grade)
 
 # AJAX ROUTES
 def preprocess(state):
@@ -87,6 +108,8 @@ def submit_mc_answer():
    if ans == int(answer["correct"]):
       global score
       score = score + 1
+      global mc_score 
+      mc_score = mc_score + 1
    return jsonify(answer)
 
 def numeric_rank(rank):
@@ -127,6 +150,8 @@ def submit_trick_answer():
    if int(ans["response"]) == int(answer["correct"]):
       global score
       score = score + 1
+      global trick_score
+      trick_score = trick_score + 1
       answer["explanation"] = 'Correct!'
    else:
       answer["explanation"] = "Incorrect. Correct cards are green"
@@ -182,6 +207,8 @@ def submit_play_answer():
    if isRight:
       global score
       score  =  score + 1
+      global play_score
+      play_score = play_score + 1
       answer["explanation"] = "Correct!"
    else:
       answer["explanation"] = "Incorrect. Correct cards are green"
